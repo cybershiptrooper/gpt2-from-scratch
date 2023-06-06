6dc14750b787f7b7850c41fd73f62b758a409a04
cybershiptrooper <cybershiptrooper@gmail.com>
2023-06-07 00:41:01 +0530

    parallel attention heads

--- model/attention.py
+++ model/attention.py
@@ -57,44 +57,96 @@ class SingleHeadAttention(nn.Module):
 class MultiHeadAttention(nn.Module):
     '''
     Implements
-    1. Delegate attention to attention heads (TODO: parallely)
+    1. Delegate attention to attention heads
     2. Wo @ (Attention heads) 
     3. Write to residual stream
     '''
-    def __init__(self, d_model, n_heads, init_range=0.02, verbose = False, save_pattern=False):
+    def __init__(self, d_model, n_heads, init_range=0.02, verbose = False, save_pattern=False, parallel=True):
         super().__init__()
         assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
         self.n_heads = n_heads
         d_head = int(d_model/n_heads)
         self.verbose = verbose
-        self.attention_heads = nn.ModuleList([
-            SingleHeadAttention(d_model, 
-                                d_head,
-                                init_range, verbose, save_pattern=save_pattern) 
-                    for _ in range(n_heads)])
+        if(not parallel):
+            self.attention_heads = nn.ModuleList([
+                SingleHeadAttention(d_model, 
+                                    d_head,
+                                    init_range, verbose, save_pattern=save_pattern) 
+                        for _ in range(n_heads)])
+        else:
+            # parallel attention heads
+            self.Wk = nn.Parameter(torch.empty(d_model, d_model))
+            self.Wv = nn.Parameter(torch.empty(d_model, d_model))
+            self.Wq = nn.Parameter(torch.empty(d_model, d_model))
+            nn.init.normal_(self.Wk, mean=0, std=init_range)
+            nn.init.normal_(self.Wv, mean=0, std=init_range)
+            nn.init.normal_(self.Wq, mean=0, std=init_range)
+            self.d_model = d_model
+
         self.Wo = nn.Parameter(torch.empty(d_model, d_model))
         nn.init.normal_(self.Wo, mean=0, std=init_range)
         self.save_pattern = save_pattern
         self.pattern = None
+        self.parallel = parallel
         
     def forward(self, residual_stream):
         if(self.verbose):
-            print("Multi attention head input shapes: ", residual_stream.shape)
-        
-        # TODO: Parallelize this!!!
-        attention_out_list = [self.attention_heads[i](residual_stream) 
-                           for i in range(self.n_heads)] # This is not parallel!!
-        attention_out = torch.cat(attention_out_list, dim=-1)
-        if(self.save_pattern):
-            self.pattern = [self.attention_heads[i].pattern for i in range(self.n_heads)]
-        if(self.verbose):
-            print("Multi attention head output shapes: ", [x.shape for x in attention_out_list])
-            print("Multi attention head concatenated shape: ", attention_out.shape)
-        
-        # each word is now a weighted sum of the other words in the sentence
-        out = attention_out @ self.Wo
-        # write to residual stream
-        out = residual_stream + out
-        if(self.verbose):
-            print("Multi attention head output shape: ", out.shape)
-        return out
+            print("Multi attention head input shape: ", residual_stream.shape)
+        if not self.parallel:            
+            attention_out_list = [self.attention_heads[i](residual_stream) 
+                            for i in range(self.n_heads)] 
+            attention_out = torch.cat(attention_out_list, dim=-1)
+            if(self.save_pattern):
+                self.pattern = [self.attention_heads[i].pattern for i in range(self.n_heads)]
+            if(self.verbose):
+                print("Multi attention head output shapes: ", [x.shape for x in attention_out_list])
+                print("Multi attention head concatenated shape: ", attention_out.shape)
+            
+            # each word is now a weighted sum of the other words in the sentence
+            out = attention_out @ self.Wo
+            # write to residual stream
+            out = residual_stream + out
+            if(self.verbose):
+                print("Multi attention head output shape: ", out.shape)
+            return out
+        else:
+            wps = residual_stream.shape[1]
+            batch_size = residual_stream.shape[0]
+            qs = torch.einsum('bwm, mo -> bwo', 
+                            residual_stream, self.Wq).reshape(
+                                (batch_size, 
+                                self.n_heads, 
+                                wps, -1))
+            vs = torch.einsum('bwm, mo -> bwo', 
+                            residual_stream, self.Wv).reshape(
+                                (batch_size, 
+                                self.n_heads, 
+                                wps, -1))
+            ks = torch.einsum('bwm, mo -> bwo', 
+                            residual_stream, self.Wk).reshape(
+                                (batch_size, 
+                                self.n_heads, 
+                                wps, -1))
+            qks = torch.einsum('...nd, ...od->...no', qs, ks) 
+            mask = torch.tril(torch.ones_like(qks))
+            mask[mask == 0] = -float('inf')
+            mask[mask == 1] = 0
+            qks = torch.tril(qks) + mask
+
+            if(self.verbose):
+                print("[parallel] Multihead attention Q, K, V shapes: ", qs.shape, ks.shape, vs.shape)
+                print("[parallel] Multihead attention Q @ K shape: ", qks.shape)
+
+            pattern = (nn.Softmax(dim=-1)( 
+                ( qks ) / ((self.d_model / self.n_heads)**.5) 
+                ))
+            
+            if(self.verbose):
+                print("[parallel] Multihead attention pattern shape: ", pattern.shape)
+
+            if(self.save_pattern):
+                self.pattern = pattern
+            out = residual_stream + (pattern @ vs).reshape((batch_size, wps, -1))
+            if(self.verbose):
+                print("[parallel] Multihead attention output shape: ", out.shape)
+            return  out
